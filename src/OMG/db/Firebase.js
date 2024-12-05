b92e5ec68c5ff72f42b7b35369775f121df85a0c
imtuhyung <mohittemporaryid@gmail.com>
2024-12-05 20:09:15 +0530

    Updated

--- src/OMG/db/Firebase.js
+++ src/OMG/db/Firebase.js
@@ -16,6 +16,7 @@ import {
 } from "firebase/firestore";
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
